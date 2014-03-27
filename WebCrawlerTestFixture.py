949cb7302f6110e00e961925474dae2e76802fe9
justinwerre <werrejustin@gmail.com>
2014-03-26 22:46:07 -0600

    added a setup and teardown function to the unit test

--- WebCrawlerTestFixture.py
+++ WebCrawlerTestFixture.py
@@ -2,12 +2,16 @@ import unittest
 from WebCrawler import WebCrawler
 
 class TestWebCrawler(unittest.TestCase):
+	def setUp(self):
+		self.spider = WebCrawler("robot", "http://en.wikipedia.org/wiki/Robots_exclusion_standard")
+
+	def tearDown(self):
+		del self.spider
+
 	def testInstantiateKeyWord(self):
-		self.spider = WebCrawler("robot", "")
 		self.assertEquals(self.spider.keyWord, "robot")
 
 	def testInstantiateWebAddress(self):
-		self.spider = WebCrawler("robot", "http://en.wikipedia.org/wiki/Robots_exclusion_standard")
 		self.assertEquals(self.spider.currentWebAddress, "http://en.wikipedia.org/wiki/Robots_exclusion_standard")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestWebCrawler)
